744b63d2ae8f4b3aa707dfd8220b42716c8b7985
Harsha8315 <harshavardhans535@gmail.com>
2026-02-09 16:21:03 +0530

    Day 1

--- src/com/stschool/ecommerce/controller/CustomerController.java
+++ src/com/stschool/ecommerce/controller/CustomerController.java
@@ -1,4 +1,9 @@
 package com.stschool.ecommerce.controller;
 
+import com.stschool.ecommerce.model.Customer;
+
 public class CustomerController {
+    public void handleSignup(Customer customer){
+        System.out.println("Handeling Sigma For : " + customer);
+    }
 }
---
+++ src/com/stschool/ecommerce/ui/CustomerMenu.java
@@ -0,0 +1,34 @@
+package com.stschool.ecommerce.ui;
+
+import com.stschool.ecommerce.model.Customer;
+
+import java.util.Scanner;
+
+public class CustomerMenu {
+    public Customer inputCustomerSignupData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Id : ");
+        String id = scanner.next();
+        System.out.println("Enter First Name : ");
+
+        String firstName = scanner.next();
+        System.out.println("Enter Last Name : ");
+        String lastName = scanner.next();
+        System.out.println("Mobile No : ");
+        String mobileNo = scanner.next();
+        System.out.println("Enter Email Id : ");
+        String emailId = scanner.next();
+        System.out.println("Enter Password : ");
+        String password = scanner.next();
+
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customer.setMobileNo(mobileNo);
+        customer.setEmailId(emailId);
+        customer.setPassword(password);
+        scanner.close();
+        return customer;
+    }
+}
