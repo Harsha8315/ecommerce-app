04244ab688d699938f605207b6e4962e9fc28736
Harsha8315 <harshavardhans535@gmail.com>
2026-04-06 12:38:38 +0530

    feat: <convert csv file into list of products

---
+++ src/com/stschool/ecommerce/util/CsvParser.java
@@ -0,0 +1,39 @@
+package com.stschool.ecommerce.util;
+
+import com.stschool.ecommerce.model.Product;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvParser {
+    /*
+    method to convert product.csv file into list of products
+     */
+    List<Product> getProductsFromCsv() throws IOException {
+        List<Product> products = new ArrayList<>();
+        File file = new File("D://products.csv");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        br.readLine();
+        String productData = br.readLine();
+        while (productData != null ){
+            String[] split = productData.split(",");
+            Product product = new Product();
+            product.setId(Integer.parseInt(split[0]))
+                    .setName(split[1])
+                    .setMaxRetailPrice(Double.parseDouble(split[2]))
+                    .setDiscountPercentage(Double.parseDouble(split[3]))
+                    .setAvailable(Boolean.parseBoolean(split[4]))
+                    .setCompany(split[5])
+                    .setCategory(split[6])
+                    .setManufacturedYear(Integer.parseInt(split[7]));
+            products.add(product);
+            productData = br.readLine();
+        }
+        return products;
+    }
+
+}
