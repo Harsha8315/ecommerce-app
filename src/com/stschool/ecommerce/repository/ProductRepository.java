6cd1fe6f381c3bc114482f1aab2065da613781c4
Harsha8315 <harshavardhans535@gmail.com>
2026-04-08 11:01:09 +0530

    feat: <implemented CRUD methods>

--- src/com/stschool/ecommerce/repository/ProductRepository.java
+++ src/com/stschool/ecommerce/repository/ProductRepository.java
@@ -1,3 +1,38 @@
 package com.stschool.ecommerce.repository;
+
+import com.stschool.ecommerce.model.Product;
+import com.stschool.ecommerce.util.CsvParser;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
 public class ProductRepository {
+   private final List<Product> products;
+
+   public ProductRepository() throws IOException{
+       CsvParser csvParser = new CsvParser();
+       products = csvParser.getProductsFromCsv();
+   }
+   public Product save(Product product){
+       this.products.add(product);
+       return product;
+   }
+   public Optional<Product> getById(int id){
+       return products.stream()
+               .filter(product -> product.getId() == id)
+               .findFirst();
+   }
+
+    public List<Product> getAll() {
+        return this.products;
+    }
+    public Product update(int id, Product product){
+       products.replaceAll(product1 -> product1.getId() == id ? product : product1);
+       return product;
+    }
+    public boolean delete(int id){
+       return products.removeIf(product -> product.getId() == id );
+    }
+
 }
